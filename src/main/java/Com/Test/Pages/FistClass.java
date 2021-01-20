fe42c8f630aadd5b4d94022d416809f14897c2d9
Satish <satish@MacBook-Pro-2.local>
2021-01-20 09:36:01 +0530

    demo commit first

---
+++ src/main/java/Com/Test/Pages/FistClass.java
@@ -0,0 +1,15 @@
+package Com.Test.Pages;
+
+public class FistClass {
+	
+	
+	
+	public void test() {
+		
+		
+		
+		System.out.println("firstclass");
+		
+	}
+
+}
---
+++ src/main/java/Com/Test/Qa/TestClass.java
@@ -0,0 +1,15 @@
+package Com.Test.Qa;
+
+public class TestClass {
+	
+	
+	
+public void print() {
+		
+		
+		
+		System.out.println("firstclass");
+		
+	}
+
+}
